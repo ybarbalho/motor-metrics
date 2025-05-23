3d2f57e719f23bb3368d05dbd8cb792f7431cc2b
ybarbalho <yuri.barbalho48@gmail.com>
2025-05-23 09:42:24 -0300

    update files for new version

--- README.md
+++ README.md
@@ -3,9 +3,20 @@
 ## Índice
 
 - [Visão Geral](#visão-geral)
+- [Funcionalidades](#funcionalidades)
+
+---
 
 ## Visão Geral
 
-Este projeto foi desenvolvido com a finalidade de observar os dados de anúncios de vendas de carros nos Estados Unidos, entre os anos de 2018 e 2019.
+O projeto Motor Metrics foi desenvolvido com a finalidade de observar dados de anúncios de vendas de carros nos Estados Unidos, entre os anos de 2018 e 2019.
 
-Em seu escopo, observamos histogramas e gráficos de dispersão das vendas de veículos no período em destaque.
+Em seu escopo, observamos gráficos relacionados às vendas e à distribuição de veículos anunciados por montadoras no período em destaque.
+
+---
+
+## Funcionalidades
+
+- Histograma de distribuição dos veículos por milhas rodadas;
+- Gráficos de dispersão de milhas rodadas por preço;
+- Distribuição de montadoras dentre os veículos anunciados.
--- app.py
+++ app.py
@@ -2,12 +2,27 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# Importando os dados
 data = pd.read_csv('vehicles_us.csv')
 
-st.header('Motor Metrics: vendas automotivas')
+# Criando uma coluna de montadoras para observação.
+data['manufacturer'] = data['model'].str.split().str[0]
 
+st.title('Motor Metrics: vendas automotivas')
+
+st.header('Visualização dos dados')
+
+# Filtragem dos dados por meio do preço dos automóveis.
+filter_view = st.slider('Filtrar o valor do anúncio:', 0,
+                        data['price'].max(), (50000, 100000))
+
+data_filtered = data[(data['price'] >= filter_view[0]) &
+                     (data['price'] <= filter_view[1])]
+
+st.write(data_filtered)
+
+# Histograma para observação dos dados por 'odometer'.
 build_histogram = st.checkbox('Criar um histograma de vendas')
-build_scatter = st.checkbox('Criar um gráfico de dispersão')
 
 if build_histogram:
     st.write('Histograma por Milhas rodadas')
@@ -17,10 +32,26 @@ if build_histogram:
 
     st.plotly_chart(fig, use_container_width=True)
 
+# Gráfico de dispersão para observação dos dados.
+build_scatter = st.checkbox('Criar um gráfico de dispersão')
+
 if build_scatter:
     st.write("Gráfico de dispersão de 'Milhas rodadas x Preço'")
 
     fig = px.scatter(data, x='odometer', y='price', labels={
                      'odometer': 'Milhas rodadas', 'price': 'Preço do automóvel'})
+
+    st.plotly_chart(fig, use_container_width=True)
+
+st.header('Total de veículos por montadora')
+
+# Gráfico de barras para observar a frequência das montadoras.
+model_viewer = st.button('Visualizar montadoras')
+
+if model_viewer:
+    frequency_df = data['manufacturer'].value_counts().reset_index()
+    frequency_df.columns = ['Montadora', 'Frequência']
+
+    fig = px.bar(frequency_df, x='Montadora', y='Frequência')
 
     st.plotly_chart(fig, use_container_width=True)
